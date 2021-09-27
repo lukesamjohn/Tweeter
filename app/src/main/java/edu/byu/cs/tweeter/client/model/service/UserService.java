ed4114f72be07e31d1fc4ae179e4e06c38f8d9ba
Luke Johnson <lukesj2011@gmail.com>
2021-09-27 15:27:01 -0600

    Finish register optimizations

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -1,14 +1,20 @@
 package edu.byu.cs.tweeter.client.model.service;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 
+import java.io.ByteArrayOutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.backgroundTask.LoginTask;
+import edu.byu.cs.tweeter.client.backgroundTask.RegisterTask;
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
@@ -18,7 +24,7 @@ import edu.byu.cs.tweeter.model.domain.User;
 public class UserService {
 
     /**
-     * Observer interface used by presenter
+     * LoginObserver interface used by presenter
      * The methods are the tasks that we want to accomplish when a User click "Login"
      */
     public interface LoginObserver {
@@ -27,37 +33,56 @@ public class UserService {
         void loginThrewException(Exception ex);
     }
 
+    /**
+     * Registration observer interface to be used by the presenter
+     * Methods are outcomes that could happen upon clicking login using the data from this Model
+     */
+    public interface RegisterObserver {
+        void registrationSucceeded(AuthToken authToken, User user);
+        void registrationFailed(String message);
+        void registrationThrewException(Exception ex);
+    }
+
 
     /**
      * Executes the login task on a new thread
      * @param alias The alias typed in by the user
      * @param password The password typed in by the user
-     * @param observer A LoginObserver used in a handler created in the presenter that passes info to the View
+     * @param loginObserver A LoginObserver used in a handler created in the presenter that passes info to the View
      */
-    public void login(String alias, String password, LoginObserver observer) {
-        LoginTask loginTask = new LoginTask(alias, password, new LoginHandler(observer));
+    public void login(String alias, String password, LoginObserver loginObserver) {
+        LoginTask loginTask = new LoginTask(alias, password, new LoginHandler(loginObserver));
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(loginTask);
     }
 
+    public void register(String firstName, String lastName, String alias, String password,
+                         ImageView image, RegisterObserver registerObserver) {
+        RegisterTask registerTask = new RegisterTask(firstName, lastName, alias, password, imageToByteArray(image), new RegisterHandler(registerObserver));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(registerTask);
+    }
+
+
+    private String imageToByteArray(ImageView imageToUpload) {
+        // Convert image to byte array.
+        Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+        byte[] imageBytes = bos.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.NO_WRAP);
+    }
+
     /**
-     * Message handler (i.e., observer) for LoginTask
+     * Message handler (i.e., Observer) for LoginTask
      */
     private static class LoginHandler extends Handler {
-
-        private final LoginObserver observer;
-        public  LoginHandler(LoginObserver observer) {
-            this.observer = observer;
+        private final LoginObserver loginObserver;
+        public  LoginHandler(LoginObserver loginObserver) {
+            this.loginObserver = loginObserver;
         }
-
-        /**
-         * Handles all the different login messages that could arise and passes them to the
-         * presenter who passes them to the view
-         * @param msg
-         */
         @Override
         public void handleMessage(@NonNull Message msg) {
-
             // handles three instances: login worked, failed, or it threw an exception
             boolean success = msg.getData().getBoolean(LoginTask.SUCCESS_KEY);
             if (success) {
@@ -68,18 +93,43 @@ public class UserService {
                 Cache.getInstance().setCurrUser(loggedInUser);
                 Cache.getInstance().setCurrUserAuthToken(authToken);
 
-
-                observer.loginSucceeded(authToken, loggedInUser);
+                loginObserver.loginSucceeded(authToken, loggedInUser);
             } else if (msg.getData().containsKey(LoginTask.MESSAGE_KEY)) {
                 String message = msg.getData().getString(LoginTask.MESSAGE_KEY);
 
-                observer.loginFailed(message);
+                loginObserver.loginFailed(message);
             } else if (msg.getData().containsKey(LoginTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
 
-                observer.loginThrewException(ex);
+                loginObserver.loginThrewException(ex);
             }
         }
     }
 
+    // RegisterHandler
+    private static class RegisterHandler extends Handler {
+        private final RegisterObserver registerObserver;
+        public RegisterHandler(RegisterObserver registerObserver) {this.registerObserver = registerObserver;}
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(RegisterTask.SUCCESS_KEY);
+            if (success) {
+                User registeredUser = (User) msg.getData().getSerializable(RegisterTask.USER_KEY);
+                AuthToken authToken = (AuthToken) msg.getData().getSerializable(RegisterTask.AUTH_TOKEN_KEY);
+
+                Cache.getInstance().setCurrUser(registeredUser);
+                Cache.getInstance().setCurrUserAuthToken(authToken);
+
+                registerObserver.registrationSucceeded(authToken, registeredUser);
+
+            } else if (msg.getData().containsKey(RegisterTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(RegisterTask.MESSAGE_KEY);
+                registerObserver.registrationFailed(message);
+
+            } else if (msg.getData().containsKey(RegisterTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(RegisterTask.EXCEPTION_KEY);
+                registerObserver.registrationThrewException(ex);
+            }
+        }
+    }
 }
--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/RegisterPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/RegisterPresenter.java
@@ -1,4 +1,97 @@
 package edu.byu.cs.tweeter.client.presenter;
 
-public class RegisterPresenter {
+import android.widget.ImageView;
+
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class RegisterPresenter implements UserService.RegisterObserver {
+    /**
+     * A View interface for the LoginFragment to implement. Contains
+     * all the functions that the presenter should be able to call on the view
+     * To communicate with the user. Now all these methods are testable without having to
+     * be able to view the screen.
+     */
+    public interface View {
+        void navigateToUser(User user);
+
+
+        void displayErrorMessage(String message);
+        void clearErrorMessage();
+
+        void displayInfoMessage(String message);
+        void clearInfoMessage();
+    }
+
+    private final RegisterPresenter.View view; // the LoginFragment that represents what is on the screen
+
+
+    /**
+     * Constructor for the LoginPresenter. Implies that every presenter has to be attaches to a
+     * view (Login Fragment)
+     * @param view A View defined by the presenter and implemented in LoginFragment
+     */
+    public RegisterPresenter(RegisterPresenter.View view) {
+        this.view = view;
+    }
+
+    public void register(String firstName, String lastName, String alias, String password, ImageView image) {
+        // clear any previous messages that were on the screen
+        view.clearErrorMessage();
+        view.clearInfoMessage();
+
+        // make sure the username and password have the proper format to be looked up in the database
+        String message = validateRegistration(firstName, lastName, alias, password, image);
+        if (message == null) {
+            view.displayInfoMessage("Hello, " + firstName + "!");
+            new UserService().register(firstName, lastName, alias, password, image, this); // start the registration routine
+        }
+        else {
+            view.displayErrorMessage("Registration failed: " + message);
+        }
+    }
+
+    private String validateRegistration(String firstName, String lastName, String alias, String password, ImageView image) {
+        if (firstName.length() == 0) {
+            return ("First Name cannot be empty.");
+        }
+        if (lastName.length() == 0) {
+            return ("Last Name cannot be empty.");
+        }
+        if (alias.length() == 0) {
+            return ("Alias cannot be empty.");
+        }
+        if (alias.charAt(0) != '@') {
+            return ("Alias must begin with @.");
+        }
+        if (alias.length() < 2) {
+            return ("Alias must contain 1 or more characters after the @.");
+        }
+        if (password.length() == 0) {
+            return ("Password cannot be empty.");
+        }
+        if (image.getDrawable() == null) {
+            return ("Profile image must be uploaded.");
+        }
+
+        return null;
+    }
+
+    @Override
+    public void registrationSucceeded(AuthToken authToken, User user) {
+        view.navigateToUser(user);
+        view.clearErrorMessage();
+        view.displayInfoMessage("Hello " + user.getName());
+    }
+
+    @Override
+    public void registrationFailed(String message) {
+        view.displayErrorMessage("Failed to register: " + message);
+    }
+
+    @Override
+    public void registrationThrewException(Exception ex) {
+        view.displayErrorMessage("Failed to register because of exception: " + ex.getMessage());
+    }
 }
